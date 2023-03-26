e88502f69f449737be02abfdae2cc554e6351309
Pedro <p.henriqueteixeira001@gmail.com>
2023-03-26 20:00:14 -0300

    80% login page complete

--- src/App.js
+++ src/App.js
@@ -1,44 +1,56 @@
 import './App.css';
 import * as React from 'react';
 import { Card, NextUIProvider } from '@nextui-org/react';
-import { Container, Row, Col, Input, Spacer, Text } from "@nextui-org/react";
+import { Container, Button, Input, Spacer, Text } from "@nextui-org/react";
 
 function loginForm() {
   return (
-  <Container fluid>
-    <Row justify='center' align='center'>  
-      <section id='loginForm'>
-          <form>
-            <Card variant='shadow'>
-              <Card.Body>
-                <Text h3 weight='hairline' align='center'>Entrar</Text>
-              <Input
-              type='text'
-              placeholder='Usuário'
-              required={true}
-              size='xl'
-              width='250px'
-              color='primary'
-              clearable={true}> 
-              </Input>
+    <div>
+    <Container display='flex' alignItems='center' justify='center' css={{minHeight:'100vh'}}>
+        <section id='loginForm'>
+            <form>
+              <Card variant='shadow' css={{
+                background: 'rgba(39, 39, 39, 0.1)',
+                backdropFilter: 'blur(20px)'}}>
+                <Card.Body>
+                  <Text h2 weight='hairline' align='center'>Entrar</Text>
+                  <Spacer y={-0.55}></Spacer>
+                  <Text h5 weight='hairline' align='center' css={{opacity: '60%'}}> 
+                  Para continuar preencha suas informações.
+                  </Text>
+                  <Spacer y={1}></Spacer>
+                <Input
+                type='text'
+                placeholder='Usuário'
+                required={true}
+                size='xl'
+                width='330px'
+                color='primary'
+                clearable={true}
+                color={{background:'red'}}
+                >
+                   
+                </Input>
 
-              <Spacer y={0.55}></Spacer>
+                <Spacer y={0.55}></Spacer>
 
-              <Input.Password
-              type='password'
-              placeholder='Senha'
-              required={true}
-              size='xl'
-              width='250px'
-              animated={true}
-              color='primary'> 
-              </Input.Password>
-              </Card.Body>
-              </Card>
-          </form> 
-      </section>
-      </Row>
-  </Container>
+                <Input.Password
+                type='password'
+                placeholder='Senha'
+                required={true}
+                size='xl'
+                width='330px'
+                animated={true}
+                color='primary'> 
+                </Input.Password>
+                <Spacer y={0.55}></Spacer>
+                <Button size='xl'>Entrar</Button>
+                </Card.Body>
+                </Card>
+            </form> 
+        </section>
+    </Container>
+  </div>
   );
 }
 
