eafcd010f8e9c8603f3fff0220d2811f4dc950f6
Pedro <p.henriqueteixeira001@gmail.com>
2023-03-26 00:50:38 -0300

    login page in development

--- src/App.js
+++ src/App.js
@@ -1,20 +1,53 @@
 import './App.css';
+import * as React from 'react';
+import { Card, NextUIProvider } from '@nextui-org/react';
+import { Container, Row, Col, Input, Spacer, Text } from "@nextui-org/react";
 
-
-function betaoRomances(){
+function loginForm() {
   return (
-    <div>
-      <h1>Betao Romances</h1>
-    </div>
-  )
+  <Container fluid>
+    <Row justify='center' align='center'>  
+      <section id='loginForm'>
+          <form>
+            <Card variant='shadow'>
+              <Card.Body>
+                <Text h3 weight='hairline' align='center'>Entrar</Text>
+              <Input
+              type='text'
+              placeholder='Usuário'
+              required={true}
+              size='xl'
+              width='250px'
+              color='primary'
+              clearable={true}> 
+              </Input>
+
+              <Spacer y={0.55}></Spacer>
+
+              <Input.Password
+              type='password'
+              placeholder='Senha'
+              required={true}
+              size='xl'
+              width='250px'
+              animated={true}
+              color='primary'> 
+              </Input.Password>
+              </Card.Body>
+              </Card>
+          </form> 
+      </section>
+      </Row>
+  </Container>
+  );
 }
 
-function App() {
+function App( {Component}) {
   return (
-    <div className="App">
-      {betaoRomances()}
-    </div>
-  );
+    <NextUIProvider>
+      {loginForm()};
+    </NextUIProvider>
+  )
 }
 
 export default App;
